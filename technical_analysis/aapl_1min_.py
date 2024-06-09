f450e6d720f58e16677d70a06473ddf985a8e159
Maximiliano Jimenez <pub.maximilianojimenez@gmail.com>
2024-06-09 13:46:23 -0600

    Saving outcome for all tests

--- technical_analysis/aapl_1min_.py
+++ technical_analysis/aapl_1min_.py
@@ -22,19 +22,59 @@ for i in range(1, len(indicators) + 1):
     comb = combinations(indicators, i)
     all_combinations.extend(comb)
 
-# Evaluate all combinations of indicators for AAPL
+# # Evaluate all combinations of indicators for AAPL
+# best_combination_aapl = None
+# best_value_aapl = -float("inf")
+#
+# for combination in all_combinations:
+#     study = optuna.create_study(direction='maximize')
+#     study.optimize(func=lambda trial: profit_calculator.profit(trial, data_aapl, combination), n_trials=1)
+#
+#     print(f"Best parameters for combination {combination}: {study.best_params}")
+#     print(f"Best value for combination {combination}: {study.best_value}")
+#
+#     if study.best_value > best_value_aapl:
+#         best_value_aapl = study.best_value
+#         best_combination_aapl = combination
+#
+# print(f"The best combination of indicators for AAPL is: {best_combination_aapl} with a value of: {best_value_aapl}")
+
 best_combination_aapl = None
 best_value_aapl = -float("inf")
+best_params_aapl = None
+
+# Diccionario para almacenar los mejores resultados para cada combinación
+results = {}
 
 for combination in all_combinations:
     study = optuna.create_study(direction='maximize')
     study.optimize(func=lambda trial: profit_calculator.profit(trial, data_aapl, combination), n_trials=1)
 
-    print(f"Best parameters for combination {combination}: {study.best_params}")
-    print(f"Best value for combination {combination}: {study.best_value}")
+    best_params = study.best_params
+    best_value = study.best_value
 
-    if study.best_value > best_value_aapl:
-        best_value_aapl = study.best_value
+    print(f"Best parameters for combination {combination}: {best_params}")
+    print(f"Best value for combination {combination}: {best_value}")
+
+    # Guardar los resultados en el diccionario
+    results[combination] = {"params": best_params, "value": best_value}
+
+    if best_value > best_value_aapl:
+        best_value_aapl = best_value
         best_combination_aapl = combination
+        best_params_aapl = best_params
 
-print(f"The best combination of indicators for AAPL is: {best_combination_aapl} with a value of: {best_value_aapl}")
+print(" ")
+print(" ")
+print(f"The best combination of indicators for APPLE is: {best_combination_aapl} with a value of: {best_value_aapl}")
+
+# Crear un diccionario con el mejor resultado
+best_outcome = {
+    "combination": best_combination_aapl,
+    "value": best_value_aapl,
+    "params": best_params_aapl
+}
+
+print(f"Best outcome: {best_outcome}")
+
+
