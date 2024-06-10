3e2809f7b8b7c4dd61c4b76cc68a6c96dfb0d938
GustavodeAnda <111828241+GustavodeAnda@users.noreply.github.com>
2024-06-10 02:09:16 -0600

    test and training btc 1 minute finished

--- technical_analysis/test_btc_1m.py
+++ technical_analysis/test_btc_1m.py
@@ -2,10 +2,8 @@ import ta
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cargar los datos
-data = pd.read_csv("./data/btc_project_test.csv").dropna()
+data = pd.read_csv("./data/btc_project_1m_test.csv").dropna()
 
-# Parámetros optimizados
 n_shares = 6.0156138225684685
 stop_loss = 0.25350468439149887
 take_profit = 0.29385546248739247
@@ -15,14 +13,12 @@ macd_sign_window = 15
 capital = 1_000_000
 COM = 0.125 / 100
 
-# Calcular el indicador MACD con las ventanas optimizadas
 macd = ta.trend.MACD(close=data["Close"], window_slow=macd_slow_window, window_fast=macd_fast_window, window_sign=macd_sign_window)
 technical_data = pd.DataFrame()
 technical_data["Close"] = data["Close"]
 technical_data["MACD"] = macd.macd()
 technical_data["MACD Signal"] = macd.macd_signal()
 
-# Generar señales de compra y venta con los umbrales optimizados
 technical_data["BUY_SIGNAL"] = (technical_data["MACD"] > technical_data["MACD Signal"])
 technical_data["SELL_SIGNAL"] = (technical_data["MACD"] < technical_data["MACD Signal"])
 
@@ -40,7 +36,6 @@ for i, row in technical_data.iterrows():
             capital += row.Close * pos["n_shares"] * (1 - COM)
             active_positions.remove(pos)
 
-    # Abrir nuevas posiciones si hay señal de compra
     if row.BUY_SIGNAL:
         if capital > row.Close * (1 + COM) * n_shares:
             capital -= row.Close * (1 + COM) * n_shares
@@ -54,19 +49,16 @@ for i, row in technical_data.iterrows():
         else:
             print("OUT OF CASH")
 
-    # Evaluar las posiciones activas y actualizar el valor del portafolio
     positions_value = sum(
         [pos["n_shares"] * row.Close for pos in active_positions if pos["type"] == "LONG"]
     )
     portfolio_value.append(capital + positions_value)
 
-# Cerrar todas las posiciones restantes al final
 for pos in active_positions.copy():
     capital += pos["n_shares"] * technical_data.iloc[-1].Close * (1 - COM)
     active_positions.remove(pos)
 portfolio_value.append(capital)
 
-# Estrategia pasiva para comparación
 capital_benchmark = 1_000_000
 shares_to_buy = capital_benchmark // (technical_data.Close.values[0] * (1 + COM))
 capital_benchmark -= shares_to_buy * technical_data.Close.values[0] * (1 + COM)
@@ -74,10 +66,8 @@ portfolio_value_benchmark = (shares_to_buy * technical_data.Close) + capital_ben
 portfolio_value_benchmark_p = pd.DataFrame(portfolio_value_benchmark)
 portfolio_value_benchmark_p.index = range(0, len(portfolio_value_benchmark))
 
-# Imprimir el valor final del portafolio activo
 print(f"Valor final del portafolio activo: {portfolio_value[-1]}")
 
-# Gráfica de comparación
 plt.figure(figsize=(14, 7))
 plt.title('Trading vs training')
 plt.plot(portfolio_value, label="Active")
--- technical_analysis/training_btc_1m.py
+++ technical_analysis/training_btc_1m.py
@@ -2,10 +2,8 @@ import ta
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cargar los datos
-data = pd.read_csv("./data/btc_project_train.csv").dropna()
+data = pd.read_csv("./data/btc_project_1m_train.csv").dropna()
 
-# Parámetros optimizados
 n_shares = 6.0156138225684685
 stop_loss = 0.25350468439149887
 take_profit = 0.29385546248739247
@@ -15,14 +13,12 @@ macd_sign_window = 15
 capital = 1_000_000
 COM = 0.125 / 100
 
-# Calcular el indicador MACD con las ventanas optimizadas
 macd = ta.trend.MACD(close=data["Close"], window_slow=macd_slow_window, window_fast=macd_fast_window, window_sign=macd_sign_window)
 technical_data = pd.DataFrame()
 technical_data["Close"] = data["Close"]
 technical_data["MACD"] = macd.macd()
 technical_data["MACD Signal"] = macd.macd_signal()
 
-# Generar señales de compra y venta con los umbrales optimizados
 technical_data["BUY_SIGNAL"] = (technical_data["MACD"] > technical_data["MACD Signal"])
 technical_data["SELL_SIGNAL"] = (technical_data["MACD"] < technical_data["MACD Signal"])
 
@@ -30,7 +26,6 @@ active_positions = []
 portfolio_value = [capital]
 
 for i, row in technical_data.iterrows():
-    # Cerrar posiciones que han alcanzado tp o sl
     active_pos_copy = active_positions.copy()
     for pos in active_pos_copy:
         if row.Close < pos["stop_loss"]:
@@ -40,7 +35,6 @@ for i, row in technical_data.iterrows():
             capital += row.Close * pos["n_shares"] * (1 - COM)
             active_positions.remove(pos)
 
-    # Abrir nuevas posiciones si hay señal de compra
     if row.BUY_SIGNAL and capital > row.Close * (1 + COM) * n_shares:
         capital -= row.Close * (1 + COM) * n_shares
         active_positions.append({
@@ -51,28 +45,23 @@ for i, row in technical_data.iterrows():
             "take_profit": row.Close * (1 + take_profit)
         })
 
-    # Evaluar las posiciones activas y actualizar el valor del portafolio
     positions_value = sum(
         [pos["n_shares"] * row.Close for pos in active_positions if pos["type"] == "LONG"]
     )
     portfolio_value.append(capital + positions_value)
 
-# Cerrar todas las posiciones restantes al final
 for pos in active_positions.copy():
     capital += pos["n_shares"] * technical_data.iloc[-1].Close * (1 - COM)
     active_positions.remove(pos)
 portfolio_value.append(capital)
 
-# Estrategia pasiva para comparación
 capital_benchmark = 1_000_000
 shares_to_buy = capital_benchmark // (technical_data.Close.values[0] * (1 + COM))
 capital_benchmark -= shares_to_buy * technical_data.Close.values[0] * (1 + COM)
 portfolio_value_benchmark = (shares_to_buy * technical_data.Close) + capital_benchmark
 
-# Imprimir el valor final del portafolio activo
 print(f"Valor final del portafolio activo: {portfolio_value[-1]}")
 
-# Gráfica de comparación
 plt.figure(figsize=(14, 7))
 plt.title('Trading vs. Buy and Hold')
 plt.plot(portfolio_value, label="Active")
