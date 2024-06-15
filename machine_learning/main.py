1ecb72ffa8de0c1944ca180f237d62c77b2fa050
GustavodeAnda <111828241+GustavodeAnda@users.noreply.github.com>
2024-06-14 22:52:40 -0600

    i just added xgb to requirements.txt

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -7,16 +7,42 @@ from sklearn.metrics import confusion_matrix, mean_squared_error
 import ta
 
 data = pd.read_csv("./data/aapl_project_train.csv").dropna()
+
 data_clean = data.loc[:, ["Close"]]
-data_clean["Y"] = data_clean.shift(-10)
+data_clean["Y"] = data_clean.shift(-15)
 data_clean["Close_t1"] = data.loc[:, ["Close"]].shift(1)
 data_clean["Close_t2"] = data.loc[:, ["Close"]].shift(2)
 data_clean["Close_t3"] = data.loc[:, ["Close"]].shift(3)
 data_clean["Close_t4"] = data.loc[:, ["Close"]].shift(4)
 data_clean["Close_t5"] = data.loc[:, ["Close"]].shift(5)
-data_clean["rsi_10"] = data.loc[:, ["Close"]].shift(6)
+data_clean["rsi_10"] = ((ta.momentum.RSIIndicator(data["Close"], window=10)).rsi())
+data_clean["rsi_20"] = ((ta.momentum.RSIIndicator(data["Close"], window=20)).rsi())
+data_clean["rsi_30"] = ((ta.momentum.RSIIndicator(data["Close"], window=30)).rsi())
+data_clean["macd_10_24_7"] = ((ta.trend.MACD(close=data["Close"], window_slow=24, window_fast=10, window_sign=7)).macd())
+data_clean["macd_12_26_9"] = ((ta.trend.MACD(close=data_clean["Close"], window_slow=26, window_fast=12, window_sign=9)).macd())
+data_clean["macd_5_35_5"] = ((ta.trend.MACD(close=data_clean["Close"], window_slow=35, window_fast=5, window_sign=5)).macd())
+
+### bollinger bands
+bollinger_20_2 = ta.volatility.BollingerBands(close=data_clean["Close"], window=20, window_dev=2)
+data_clean["bollinger_20_2_hband"] = bollinger_20_2.bollinger_hband()
+data_clean["bollinger_20_2_lband"] = bollinger_20_2.bollinger_lband()
+data_clean["bollinger_20_2_mavg"] = bollinger_20_2.bollinger_mavg()
+
+bollinger_10_1_5 = ta.volatility.BollingerBands(close=data_clean["Close"], window=10, window_dev=1.5)
+data_clean["bollinger_10_1_5_hband"] = bollinger_10_1_5.bollinger_hband()
+data_clean["bollinger_10_1_5_lband"] = bollinger_10_1_5.bollinger_lband()
+data_clean["bollinger_10_1_5_mavg"] = bollinger_10_1_5.bollinger_mavg()
+
+bollinger_50_2_5 = ta.volatility.BollingerBands(close=data_clean["Close"], window=50, window_dev=2.5)
+data_clean["bollinger_50_2_5_hband"] = bollinger_50_2_5.bollinger_hband()
+data_clean["bollinger_50_2_5_lband"] = bollinger_50_2_5.bollinger_lband()
+data_clean["bollinger_50_2_5_mavg"] = bollinger_50_2_5.bollinger_mavg()
 data_clean = data_clean.dropna()
-data_clean.head()
+
+data_clean["atr_14"] = (ta.volatility.AverageTrueRange(high=data["High"], low=data["Low"], close=data["Close"], window=14)).average_true_range()
+data_clean["atr_10"] = (ta.volatility.AverageTrueRange(high=data["High"], low=data["Low"], close=data["Close"], window=10)).average_true_range()
+data_clean["atr_20"] = (ta.volatility.AverageTrueRange(high=data["High"], low=data["Low"], close=data["Close"], window=20)).average_true_range()
+
 
 ## Classification
 
