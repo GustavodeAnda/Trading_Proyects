b92caa9e206e3a760e40fb2056c1ace7fd2f09f4
GustavodeAnda <111828241+GustavodeAnda@users.noreply.github.com>
2024-06-18 16:02:06 -0600

    xgb ready to use

--- machine_learning/Logistic_regression_opt_buy_signal.py
+++ machine_learning/Logistic_regression_opt_buy_signal.py
@@ -146,7 +146,6 @@ study.optimize(objective, n_trials=50)
 print("Best trial:", study.best_trial.number)
 print("Best value:", study.best_trial.value)
 print("Best hyperparameters:", study.best_params)
-study.best_params
 
 files = reading_files(list_of_equity)
 data = files["./data/aapl_project_test.csv"]
